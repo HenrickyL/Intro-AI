a15907ea94accd1f0a8c3850d4267eadf41c0c35
HenrickyL <Henrickyl1@gmail.com>
2022-09-16 18:53:45 -0300

    upload

--- 2.Agentes de resolução de Problemas/1.Modelos/main.py
+++ 2.Agentes de resolução de Problemas/1.Modelos/main.py
@@ -1,23 +1,7 @@
-from state import State
 from map import Map
-from transition import Transition
-with open('data.txt','r') as file:
-    states = []
-    for line in file.readlines():
-        city, city_targets = line.split(':')
-        city = city.strip()
-        city_targets = city_targets.strip()
-
-        edges = []
-        if(city_targets):
-            for transiction in city_targets.split(','):
-                target, cost = transiction.strip().split('-')
-                edges.append(Transition(target=target, cost=cost))
-        state = State(city, edges)
-        state.show()
-        states.append(state)
-map = Map(states)
-print(map)
+map = Map.readData('data.txt')
+map.showStates()
+map.showCitys()
 map.draw(layout='rt')
 
     
--- 2.Agentes de resolução de Problemas/1.Modelos/map.py
+++ 2.Agentes de resolução de Problemas/1.Modelos/map.py
@@ -1,8 +1,32 @@
 from state import State
 from igraph import *
+from transition import Transition
 class Map:
     def __init__(self, states:list[State]):
         self.states = states.copy()
+    def showStates(self):
+        for state in self.states:
+            state.show()
+    def showCitys(self):
+        setStates = {state.name:i for i,state in zip(range(len(self.states)),self.states)}
+        print(setStates)
+    def readData(data:str):
+        with open(data,'r') as file:
+            states = []
+            for line in file.readlines():
+                city, city_targets = line.split(':')
+                city = city.strip()
+                city_targets = city_targets.strip()
+
+                edges = []
+                if(city_targets):
+                    for transiction in city_targets.split(','):
+                        target, cost = transiction.strip().split('-')
+                        edges.append(Transition(target=target, cost=cost))
+                state = State(city, edges)
+                
+                states.append(state)
+        return Map(states)
     def draw(self, layout=None):
         g=Graph()
         g.add_vertices(len(self.states))
@@ -10,7 +34,6 @@ class Map:
         edge = []
         weights = []
         setStates = {state.name:i for i,state in zip(range(len(self.states)),self.states)}
-        print('>>',setStates)
         get = lambda name: setStates.get(name)
         for state in self.states:
             g.vs[i]["id"]= get(state.name)
@@ -19,7 +42,6 @@ class Map:
                 edge.append((get(state.name),get(e.target)))
                 weights.append(e.cost)
             i +=1
-        print(edge)
         g.add_edges(edge)
         g.es['weight'] = weights
         g.es['label'] = weights
